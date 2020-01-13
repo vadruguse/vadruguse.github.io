d8fbaafdd3bec27d2f544fe237f5ba0473143f31
kunalarora <kunalarora199494@gmail.com>
2020-01-13 02:10:10 +0100

    Refactor code variables and structure

--- sketch.js
+++ sketch.js
@@ -1,46 +1,39 @@
-let data;
+let total_data;
+let affected_data;
+let drug_data = [];
 let drug_name;
+let hover_position = [];
+let drugArrays = []; // 3-dimentional array for plotting venn diagram
+let age_chart_state = -1;
+let drawCrosses = false
 
 let canvasWidth = 1450;
 let canvasHeight = 750;
-let scaling = 6;
+let total_circle_scaling = 6;
 let rect_scaling = 0.5;
-let rectWidth = 120;
-let rectHeight = 150;
 
-let circleArray = [100, 335, 578, 818, 1055];
-let drugArray = [70,320,565,785,1020];
-let rectArray = [400,160+380,280+400,420+400,560+400];
-let toplegend = [265, 315, 365]
+// Position
+let total_n_addicted_circle_position = [100, 335, 578, 818, 1055];
+let drug_name_position = [70,320,565,785,1020];
+let age_legend_rect_position = [400,540,680,820,960];
+let age_chart_rectangle_position = [200+400, 400+150, 400, 50]
 
-//let colorArray = ['orange','white','green'];
-// let colorArray = ['rgba(255, 0, 0, 1)', 'rgba(0, 255, 0, 1)','rgba(0, 0, 255, 1)']
+// Color
+let age_chart_color = ['#00FF00', 'yellow', '#D2691E', '#7FFFD4']
 
-let colorArray = ['#FFE4B5','#B0C4DE','#FFB6C1']
 
-let rArray = [];
-let drugArrays = [];
-let offset = 0;
-let state = -1;
-let colorAge = ['#00FF00', 'yellow', '#D2691E', '#7FFFD4']
+let age_chart_category = ['Crime', 'Mental Health', 'Unemployment']
+let age_legend = ['18-25 years', '26-34 years','35-49 years', '50+ years']
+
+
 let fileNames = ['marijuana_category.csv', 'cocaine_category.csv', 'heroin_category.csv', 'hallucinogen_category.csv', 'methamphetamine_category.csv']
-let drugData = []
-let graphVal = [200+400, 400+150, 400, 50]
-let categoryArray = ['Crime', 'Mental Health', 'Unemployment']
-let ageArray = ['18-25 years', '26-34 years','35-49 years', '50+ years']
-let drawCrosses = false
-let toggleBox = []
-
-let x=0;
-
 function preload() {
-  data = loadTable('data/real_data.csv','csv','header')
-  // category_data = loadTable('real_categorydata.csv','csv','header')
+  total_data = loadTable('real_data.csv','csv','header')
 
-  ellipse_data = loadTable('data/ellipse_data.csv', 'csv', 'header')
+  affected_data = loadTable('ellipse_data.csv', 'csv', 'header')
 
   for(let drug=0;  drug<=4;drug++){
-    drugData[drug] = loadTable('data/'+fileNames[drug], 'csv', 'header')
+    drug_data[drug] = loadTable(fileNames[drug], 'csv', 'header')
   }
 }
 
@@ -58,7 +51,7 @@ function setup() {
       for(let cat=0;cat<=2;cat++){
         drugArrays[drug][cat]=[]
         for(let age=0;age<=3;age++) {
-          drugArrays[drug][cat][age] = drugData[drug].get(cat, age+1)
+          drugArrays[drug][cat][age] = drug_data[drug].get(cat, age+1)
         }
       }
   }
@@ -105,58 +98,37 @@ function draw() {
   fill(0);
   rect(50, 100, 40, 20);
   fill(50);
-  rect(10, 100, 40, 20);    
+  rect(10, 100, 40, 20);
   }
   fill(0);
   textSize(14);
   for(i=0; i<5;i++){
 
-    let drug = data.get(i+1, 'drug')
-    let drug_users = data.get(i+1,'total_users')
-    let drug_addicts = data.get(i+1,'addicted_users')
+    let drug = total_data.get(i+1, 'drug')
+    let drug_users = total_data.get(i+1,'total_users')
+    let drug_addicts = total_data.get(i+1,'addicted_users')
     let addicted_to_total = round((drug_addicts/drug_users) * 100)
-
-
-    
 
     //write drug name below
     fill(0);
-    text(drug,drugArray[i],405);
+    text(drug,drug_name_position[i],405);
 
     // outer circle
     let c1 = color('#008000');
     fill(c1);
-    t = (log(drug_users)) * scaling;
-    rArray[i] = t;
-    circle(circleArray[i],170,t);
+    t = (log(drug_users)) * total_circle_scaling;
+    hover_position[i] = t;
+    circle(total_n_addicted_circle_position[i],170,t);
 
     // inner circle
     let c2 = color('white');
     fill(c2);
     //r = addicted_to_total;
     r = t*((drug_addicts/drug_users) **0.5);
-    circle(circleArray[i],170,r);
+    circle(total_n_addicted_circle_position[i],170,r);
 
-    // draw rectangles
-//     let offset = 0;
-//     for(let k=0;k<3;k++) {
-//       let k1 = color(colorArray[k]);
-//       let k2 = color('black');
-//       if(state>=0 && i!=state){
-//         k1.setAlpha(100);
-//         k2.setAlpha(100);
-//       }
-//       fill(k1);
 
-//       let height = category_data.get(i,k+1) * rectHeight
-
-//       rect(rectArray[i],230+offset, rectWidth, height);
-
-//       fill(k2);
-//       text(round(100*category_data.get(i,k+1))+'%', rectArray[i]+0.35*rectWidth, 230+offset + 0.67*height);
-//       offset += height;
-//     }
-    if(!drawCrosses){
+  if(!drawCrosses){
       colorMode(RGB, 100);
       fill(100,0,0,15);
       ellipse(100+(i*240),260,120,120)
@@ -165,13 +137,13 @@ function draw() {
       fill(0,0,100,15);
       ellipse(140+(i*240),330,120,120)
       fill(0)
-      text(ellipse_data.get(i,'crime'),93+(i*240),245)
-      text(ellipse_data.get(i,'mental'),30+(i*240),350)
-      text(ellipse_data.get(i,'unemployment'),150+(i*240),350)
-      text(ellipse_data.get(i,'crime_mental'),62+(i*240),292)
-      text(ellipse_data.get(i,'crime_unemployment'),125+(i*240),292)
-      text(ellipse_data.get(i,'mental_unemployment'),95+(i*240),350)
-      text(ellipse_data.get(i,'INTERSECTION'),95+(i*240),310)
+      text(affected_data.get(i,'crime'),93+(i*240),245)
+      text(affected_data.get(i,'mental'),30+(i*240),350)
+      text(affected_data.get(i,'unemployment'),150+(i*240),350)
+      text(affected_data.get(i,'crime_mental'),62+(i*240),292)
+      text(affected_data.get(i,'crime_unemployment'),125+(i*240),292)
+      text(affected_data.get(i,'mental_unemployment'),95+(i*240),350)
+      text(affected_data.get(i,'INTERSECTION'),95+(i*240),310)
     }
     else{
       colorMode(RGB, 100);
@@ -182,14 +154,13 @@ function draw() {
       fill(0,0,100,15);
       ellipse(140+(i*240),330,120,120)
       fill(0)
-      let values = [round(100*ellipse_data.get(i,'crime')/ellipse_data.get(i,'UNION')),
-                    round(100*ellipse_data.get(i,'mental')/ellipse_data.get(i,'UNION')),
-                    round(100*ellipse_data.get(i,'unemployment')/ellipse_data.get(i,'UNION')),
-                    round(100*ellipse_data.get(i,'crime_mental')/ellipse_data.get(i,'UNION')),
-                    round(100*ellipse_data.get(i,'crime_unemployment')/ellipse_data.get(i,'UNION')),
-                    round(100*ellipse_data.get(i,'mental_unemployment')/ellipse_data.get(i,'UNION')),
-                    round(100*ellipse_data.get(i,'INTERSECTION')/ellipse_data.get(i,'UNION'))];
-      //console.log(values)
+      let values = [round(100*affected_data.get(i,'crime')/affected_data.get(i,'UNION')),
+                    round(100*affected_data.get(i,'mental')/affected_data.get(i,'UNION')),
+                    round(100*affected_data.get(i,'unemployment')/affected_data.get(i,'UNION')),
+                    round(100*affected_data.get(i,'crime_mental')/affected_data.get(i,'UNION')),
+                    round(100*affected_data.get(i,'crime_unemployment')/affected_data.get(i,'UNION')),
+                    round(100*affected_data.get(i,'mental_unemployment')/affected_data.get(i,'UNION')),
+                    round(100*affected_data.get(i,'INTERSECTION')/affected_data.get(i,'UNION'))];
       text(values[0]+"%",93+(i*240),245)
       text(values[1]+"%",30+(i*240),350)
       text(values[2]+"%",150+(i*240),350)
@@ -199,7 +170,7 @@ function draw() {
       text(values[5]+"%",95+(i*240),350)
       text(values[6]+"%",95+(i*240),310)
     }
-    rect(210+(i*240), 400-rect_scaling*ellipse_data.get(i,'UNION'), 10, rect_scaling*ellipse_data.get(i,'UNION'));
+    rect(210+(i*240), 400-rect_scaling*affected_data.get(i,'UNION'), 10, rect_scaling*affected_data.get(i,'UNION'));
 
   }
 
@@ -207,32 +178,31 @@ function draw() {
   // This is for tooltip
   for(let j=0; j<5;j++){
 
-    let drug_users = data.get(j+1,'total_users')
-    let drug_addicts = data.get(j+1,'addicted_users')
+    let drug_users = total_data.get(j+1,'total_users')
+    let drug_addicts = total_data.get(j+1,'addicted_users')
     let addicted_to_total = round((drug_addicts/drug_users) * 100)
 
-    if((mouseX-circleArray[j])**2 +
-       (mouseY-190)**2 <= (rArray[j]/2)**2) {
+    if((mouseX-total_n_addicted_circle_position[j])**2 +
+       (mouseY-190)**2 <= (hover_position[j]/2)**2) {
       mouseOnBox = true;
       fill(255);
       if(j==4){
         fill(255);
-        rect(0.99*circleArray[j] ,93,160,90);
+        rect(0.99*total_n_addicted_circle_position[j] ,93,160,90);
         fill(1);
-        text(('Drug: ' + data.get(j+1,'drug')), circleArray[j]-5,110);
-      text(('# drug users: ' + drug_users), circleArray[j] - 5,130);
-      text(('# drug addicts: ' + drug_addicts), circleArray[j]-5,150);
-      text(('% of addicts: ' + addicted_to_total + '%'), circleArray[j]-5,170);
+        text(('Drug: ' + total_data.get(j+1,'drug')), total_n_addicted_circle_position[j]-5,110);
+      text(('# drug users: ' + drug_users), total_n_addicted_circle_position[j] - 5,130);
+      text(('# drug addicts: ' + drug_addicts), total_n_addicted_circle_position[j]-5,150);
+      text(('% of addicts: ' + addicted_to_total + '%'), total_n_addicted_circle_position[j]-5,170);
       } else {
         fill(255);
-        rect(circleArray[j] + 0.6*rArray[j] ,110,130,90);
+        rect(total_n_addicted_circle_position[j] + 0.6*hover_position[j] ,110,130,90);
         fill(1);
-        text(('Drug: ' + data.get(j+1,'drug')), circleArray[j] + 0.6*rArray[j] + 5,125);
-      text(('# drug users: ' + drug_users), circleArray[j] + 0.6*rArray[j] + 5,145);
-      text(('# drug addicts: ' + drug_addicts), circleArray[j] + 0.6*rArray[j] + 5,165);
-      text(('% of addicts: ' + addicted_to_total + '%'), circleArray[j] + 0.6*rArray[j] + 5,185);
+        text(('Drug: ' + total_data.get(j+1,'drug')), total_n_addicted_circle_position[j] + 0.6*hover_position[j] + 5,125);
+      text(('# drug users: ' + drug_users), total_n_addicted_circle_position[j] + 0.6*hover_position[j] + 5,145);
+      text(('# drug addicts: ' + drug_addicts), total_n_addicted_circle_position[j] + 0.6*hover_position[j] + 5,165);
+      text(('% of addicts: ' + addicted_to_total + '%'), total_n_addicted_circle_position[j] + 0.6*hover_position[j] + 5,185);
       }
-      //rectDiv = rect(circleArray[j] + 0.6*rArray[j] ,110,130,90);
     }
   }
   // if(mouseOnBox){
@@ -243,7 +213,7 @@ function draw() {
   // }
 
   // fill the below rectangles
-  if(state>=0){
+  if(age_chart_state>=0){
     drawingContext.setLineDash([10, 25])
     line(390,450,1400,450)
     drawingContext.setLineDash([])
@@ -258,47 +228,33 @@ function draw() {
     textSize(14);
 
     for(let cat=0;cat<=2;cat++) {
-      let x = graphVal[0];
-      let y = graphVal[1] + cat * 60
+      let x = age_chart_rectangle_position[0];
+      let y = age_chart_rectangle_position[1] + cat * 60
       fill(0);
-      text(categoryArray[cat], x-150, y+graphVal[3]/2)
+      text(age_chart_category[cat], x-150, y+age_chart_rectangle_position[3]/2)
       for(let age=0;age<=3;age++) {
-        let agecolor = color(colorAge[age]);
+        let agecolor = color(age_chart_color[age]);
         agecolor.setAlpha(200);
         fill(agecolor)
 
-        let width = graphVal[2]*drugArrays[state][cat][age]
-        rect(x,y,width,graphVal[3])
-        if(drugArrays[state][cat][age]>0.05){
+        let width = age_chart_rectangle_position[2]*drugArrays[age_chart_state][cat][age]
+        rect(x,y,width,age_chart_rectangle_position[3])
+        if(drugArrays[age_chart_state][cat][age]>0.05){
           fill(1);
-          text(round(100*drugArrays[state][cat][age])+'%', x+0.35*width, y + 0.67*graphVal[3]);
+          text(round(100*drugArrays[age_chart_state][cat][age])+'%', x+0.35*width, y + 0.67*age_chart_rectangle_position[3]);
         }
         x+=width
       }
     }
 
     //draw legend for bottom rectangles
-    for (index = 0; index < colorAge.length; index++) {
-      fill(colorAge[index]);
-      rect(rectArray[rectArray.length-1]+250, 560+50*index, 60, 10);
+    for (index = 0; index < age_chart_color.length; index++) {
+      fill(age_chart_color[index]);
+      rect(age_legend_rect_position[age_legend_rect_position.length-1]+250, 560+50*index, 60, 10);
       fill(1);
-      text(ageArray[index], rectArray[rectArray.length-1]+320, 568+50*index)
+      text(age_legend[index], age_legend_rect_position[age_legend_rect_position.length-1]+320, 568+50*index)
     }
   }
-
-  //draw legend for top rectangles
-  // for (index = 0; index < colorArray.length; index++) {
-  //   stroke(colorArray[index]);
-  //   strokeWeight(4);
-  //   //drawingContext.setLineDash([10, 5]);
-  //   line(1180, toplegend[index], 1250, toplegend[index]);
-  //   //drawingContext.setLineDash([]);
-  //   // rect(rectArray[rectArray.length-1]+200, 250+50*index, 100, 30);
-  //   strokeWeight(1);
-  //   stroke(0)
-  //   fill(1);
-  //   text(categoryArray[index], rectArray[rectArray.length-1]+320, 270+50*index)
-  // }
 
   //top circles
     colorMode(RGB, 100);
@@ -309,10 +265,10 @@ function draw() {
     fill(0,0,100,15);
     ellipse(80+(i*240),330,50,50)
     stroke(0)
-    line(circleArray[circleArray.length-1]+210, 290, circleArray[circleArray.length-1]+280, 290)
-    line(circleArray[circleArray.length-1]+230, 330, circleArray[circleArray.length-1]+280, 330)
-    line(circleArray[circleArray.length-1]+185, 370, circleArray[circleArray.length-1]+280, 370)
-    line(circleArray[circleArray.length-1]+185, 350, circleArray[circleArray.length-1]+185, 370)
+    line(total_n_addicted_circle_position[total_n_addicted_circle_position.length-1]+210, 290, total_n_addicted_circle_position[total_n_addicted_circle_position.length-1]+280, 290)
+    line(total_n_addicted_circle_position[total_n_addicted_circle_position.length-1]+230, 330, total_n_addicted_circle_position[total_n_addicted_circle_position.length-1]+280, 330)
+    line(total_n_addicted_circle_position[total_n_addicted_circle_position.length-1]+185, 370, total_n_addicted_circle_position[total_n_addicted_circle_position.length-1]+280, 370)
+    line(total_n_addicted_circle_position[total_n_addicted_circle_position.length-1]+185, 350, total_n_addicted_circle_position[total_n_addicted_circle_position.length-1]+185, 370)
     fill(0)
     text('Crime', 1345, 294)
     text('Unemployment', 1345, 334)
@@ -322,27 +278,27 @@ function draw() {
   // outer circle
     let c1 = color('#008000');
     fill(c1);
-    circle(circleArray[circleArray.length-1]+180,190,50);
+    circle(total_n_addicted_circle_position[total_n_addicted_circle_position.length-1]+180,190,50);
 
     // inner circle
     let c2 = color('white');
     fill(c2);
-    circle(circleArray[circleArray.length-1]+180,190,25);
+    circle(total_n_addicted_circle_position[total_n_addicted_circle_position.length-1]+180,190,25);
 
     stroke(1);
     fill(1);
     textSize(11);
-    line(circleArray[circleArray.length-1]+180, 170, circleArray[circleArray.length-1]+220, 150);
-    text('Area of Outer Circle represents the \nnumber of Drug Users on Log Scale', circleArray[circleArray.length-1]+225, 155);
-    line(circleArray[circleArray.length-1]+180, 195, circleArray[circleArray.length-1]+220, 210);
-    text('Area of Inner Circle represents the \nfraction of all drug users who are addicted', circleArray[circleArray.length-1]+225, 220);
+    line(total_n_addicted_circle_position[total_n_addicted_circle_position.length-1]+180, 170, total_n_addicted_circle_position[total_n_addicted_circle_position.length-1]+220, 150);
+    text('Area of Outer Circle represents the \nnumber of Drug Users on Log Scale', total_n_addicted_circle_position[total_n_addicted_circle_position.length-1]+225, 155);
+    line(total_n_addicted_circle_position[total_n_addicted_circle_position.length-1]+180, 195, total_n_addicted_circle_position[total_n_addicted_circle_position.length-1]+220, 210);
+    text('Area of Inner Circle represents the \nfraction of all drug users who are addicted', total_n_addicted_circle_position[total_n_addicted_circle_position.length-1]+225, 220);
     textSize(14);
 }
 
 function mouseClicked() {
 
   let onBox = false;
- 
+
 
   for(let i=0;i<=4;i++) {
     // if(mouseX > (rectArray[i]) &&
@@ -353,15 +309,15 @@ function mouseClicked() {
        ((mouseX-(60+i*240))**2 + (mouseY-330)**2 <= 60**2) ||
        ((mouseX-(140+i*240))**2 + (mouseY-330)**2 <= 60**2)
       ){
-        state = i;
+        age_chart_state = i;
         cursor('grab');
         onBox = true;
-        drug_name = data.get(i+1, 'drug')
+        drug_name = total_data.get(i+1, 'drug')
         break;
      }
    }
   if(!onBox){
-    state = -1;
+    age_chart_state = -1;
     drug_name= ''
   }
   if(mouseX>=10 && mouseX <= 50 && mouseY>=100 && mouseY<=120){
